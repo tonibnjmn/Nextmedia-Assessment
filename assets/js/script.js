a364e835f4980716a92fa03d07d808d00b5ef5a9
tonibnjmn <benjaminmaritoni@gmail.com>
2025-02-17 03:04:54 +0800

    Initial commit

---
+++ assets/js/script.js
@@ -0,0 +1,39 @@
+document.addEventListener("DOMContentLoaded", function () {
+  new Swiper(".other-product-slide", {
+    loop: true,
+    spaceBetween: 35,
+    slidesPerView: 1,
+
+    autoplay: {
+      delay: 3500,
+      disableOnInteraction: false,
+    },
+
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+
+    speed: 800,
+    centeredSlides: true,
+    grabCursor: true,
+
+    breakpoints: {
+      640: { slidesPerView: 1 },
+      768: { slidesPerView: 2 },
+      1024: { slidesPerView: 3 },
+    },
+  });
+});
+
+AOS.init();
+
+AOS.init({
+  offset: 120,
+  delay: 0,
+  duration: 700,
+  easing: "ease",
+  once: false,
+  mirror: false,
+  anchorPlacement: "top-bottom",
+});
